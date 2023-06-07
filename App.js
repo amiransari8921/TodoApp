a99c2f26992fd0dd5074367d17ae134b2775338a
amiransari8921 <amir.a@nighthack.in>
2023-06-07 19:27:41 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+// import SafeAreaView from safeArea context
+// import { SafeAreaProvider } from "react-native-safe-area-context";
+//import
+//import TodoHeader
+import TodoHeader from "./src/components/TodoHeader";
+//import TodoList
+
+import TodoList from "./src/components/TodoList";
+
+//import store from "./src
+import store from "./src/redux/store";
+//import Provider
+import { Provider } from "react-redux";
+
+export default function App() {
+  const RootApp = () => {
+    return (
+      <SafeAreaView>
+        <TodoHeader />
+        <TodoList />
+      </SafeAreaView>
+    );
+  };
+
+  return (
+    <Provider store={store}>
+      <RootApp />
+    </Provider>
+  );
+}
